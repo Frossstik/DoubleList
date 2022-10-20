c955fe7fd5ff40f3ef99dc5efd5c23175ec091fb
Frossstik <lehapoh@gmail.com>
2022-10-20 13:41:18 +0300

    .

---
+++ DoubleList/Minion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garbage
+{
+    class Minion : IComparable
+    {
+        private static int ID = 1;
+        public int id { get; private set; }
+
+        public String name { get; set; }
+        public int age { get; set; }
+        public Minion()
+        {
+            id = ID;
+            ID++;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Minion m = obj as Minion;
+
+            if (m != null)
+            {
+                if (this.age < m.age)
+                {
+                    return -1;
+                }
+                else if (this.age > m.age)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                throw new Exception("Хуита!");
+            }
+        }
+    }
+}
---
+++ DoubleList/Stack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garbage
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        private T[] items;
+        private int count;
+        const int n = 1;
+        public Stack()
+        {
+            items = new T[n];
+        }
+        public Stack(int length)
+        {
+            items = new T[length];
+        }
+
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public T Current => throw new NotImplementedException();
+
+        public void Push(T item)
+        {
+            // увеличиваем стек
+            if (count == items.Length)
+                Resize(items.Length + 1);
+
+            items[count++] = item;
+        }
+        public T Pop()
+        {
+            // если стек пуст, выбрасываем исключение
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            T item = items[--count];
+            items[count] = default(T); // сбрасываем ссылку
+
+            if (count > 0 && count < items.Length - 1)
+                Resize(items.Length - 1);
+
+            return item;
+        }
+
+        private void Resize(int max)
+        {
+            T[] tempItems = new T[max];
+            for (int i = 0; i < count; i++)
+                tempItems[i] = items[i];
+            items = tempItems;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                yield return items[i];
+            }
+        }
+    }
+}
